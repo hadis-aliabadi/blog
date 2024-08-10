a21cc24914480de7c43527458d54fe777e9371b1
Hadis <hadis.eslami73@gmail.com>
2024-08-10 17:42:39 +0330

    feat:user create added

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {
+  createUser,
+ 
+} from "../controller/UserController.js";
+
+const router = Router();
+
+router.post("/", createUser);
+
+
+export default router;
